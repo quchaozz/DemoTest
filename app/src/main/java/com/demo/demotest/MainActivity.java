9470cffeb1eea0d3600b28c203946871b9a96747
379653581@qq.com <qczaeqsmqy>
2016-08-22 15:35:24 +0800

    dadadadada

--- app/src/main/java/com/demo/demotest/MainActivity.java
+++ app/src/main/java/com/demo/demotest/MainActivity.java
@@ -19,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 //Git Test
                 //woowowowowo
+                //dadadadadad
             }
         });
     }
